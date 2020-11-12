6b42a0b564d9a9db35f77fd98828225f1eeab60b
yangyang <1434753182@qq.com>
2020-11-12 18:30:28 +0800

    自定义hook

--- src/components/inner.js
+++ src/components/inner.js
@@ -3,21 +3,25 @@ import { useParams } from "react-router-dom";
 import List from "./list";
 import FooterNav from "./footerNav";
 import { useDispatch, useSelector } from "react-redux";
-import { toLoad } from "../store/action";
+// import { toLoad } from "../store/action";
+import { useLoad } from "../store/action";
 
 function Inner() {
   let { type, page } = useParams();
 
-  let disptach = useDispatch();
+  // let disptach = useDispatch();
+
+  let toLoad = useLoad();
 
   let load = useSelector((state) => {
     return state.load;
   });
 
   useEffect(() => {
-    disptach((disptach) => {
+    /*     disptach((disptach) => {
       toLoad(disptach, type, page);
-    });
+    }); */
+    toLoad(page, type);
   }, [type, page]);
 
   return (
--- src/store/action.js
+++ src/store/action.js
@@ -1,17 +1,33 @@
-import getData from "../http";
-
-function toLoad(dispatch, type, page) {
-  disptach({
-    type: "load_start",
-  });
-
-  getData(type, page).then((res) => {
-    console.log("数据请求成功", res.data);
-    disptach({
-      type: "load_end",
-      data: res.data.data,
-    });
-  });
+import getData from "../http/index";
+import {useDispatch} from "react-redux";
+// function toLoad(dispatch,type,page){
+//     dispatch({
+//         type:"load_start"
+//     })
+//     getData(type,page)
+//         .then((res)=>{
+//             console.log(res);
+//             dispatch({
+//                 type:"load_end",
+//                 data: res.data.data
+//             });
+//         });
+// }
+function useLoad(){
+    let dispatch = useDispatch();
+    return function(page,type){
+        dispatch({
+            type:"load_start"
+        })
+        getData(type,page)
+            .then((res)=>{
+                console.log(res);
+                dispatch({
+                    type:"load_end",
+                    data: res.data.data
+                });
+            });
+    }
 }
 
-export default { toLoad };
+export {useLoad}
